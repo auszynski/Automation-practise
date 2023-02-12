a4608c1f06502fe04b9b53fd2341f33350a33f8b
Lysech <arturuszynski13@gmail.com>
2023-02-12 21:38:30 +0100

    feat: navigate to first product details

--- TC8.py
+++ TC8.py
@@ -49,6 +49,57 @@ products = driver.find_element(By.XPATH,'//*[@id="header"]/div/div/div/div[2]/di
 allproducts = driver.find_element(By.XPATH,'/html/body/section[2]/div/div/div[2]/div/h2')
 if allproducts.is_displayed():
     print('User is navigated to ALL PRODUCTS page')
-
+time.sleep(3)
 # 6. The products list is visible
 
+productlist = driver.find_element(By.CSS_SELECTOR,'body > section:nth-child(3) > div.container > div > div.col-sm-9.padding-right')
+
+if productlist.is_displayed():
+    print('Product list is visible')
+time.sleep(2)
+# 7. Click on 'View Product' of first product
+
+firstproduct = driver.find_element(By.CSS_SELECTOR,'body > section:nth-child(3) > div.container > div > div.col-sm-9.padding-right > div > div:nth-child(3) > div > div.choose > ul > li > a').click()
+time.sleep(1)
+
+# 8. User is landed to product detail page
+
+firstproductdetail = driver.find_element(By.CSS_SELECTOR,'body > section > div > div > div.col-sm-9.padding-right > div.product-details')
+if firstproductdetail.is_displayed():
+    print('User is landed to product detail prage')
+
+time.sleep(1)
+
+# 9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
+
+name = driver.find_element(By.CSS_SELECTOR,'body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > h2')
+if name.is_displayed():
+    print('Name is visible')
+time.sleep(1)
+
+category = driver.find_element(By.CSS_SELECTOR,'body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > p:nth-child(3)')
+if category.is_displayed():
+    print('Category is visible')
+time.sleep(1)
+
+price = driver.find_element(By.CSS_SELECTOR,'body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > span > span')
+if price.is_displayed():
+    print('Price is visible')
+time.sleep(1)
+
+availability = driver.find_element(By.CSS_SELECTOR,'body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > p:nth-child(6)')
+if availability.is_displayed():
+    print('Availability is visible')
+time.sleep(1)
+
+condition = driver.find_element(By.CSS_SELECTOR,'body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > p:nth-child(7) > b')
+if condition.is_displayed():
+    print('Condition is visible')
+time.sleep(1)
+
+brand = driver.find_element(By.CSS_SELECTOR,'body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > p:nth-child(8)')
+if brand.is_displayed():
+    print('Brand is visible')
+time.sleep(1)
+
+driver.quit()
