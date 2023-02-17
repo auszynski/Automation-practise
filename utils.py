5763d837f08772934a5dd7ebf2793de5cc422781
Lysech <arturuszynski13@gmail.com>
2023-02-17 15:28:24 +0100

    feat: add first click to prevent ads

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+from selenium.webdriver.common.by import By
+
 def hide_ads(driver):
   print("Remove ads")
   driver.execute_script("[...document.querySelectorAll('.adsbygoogle')].map(el => el.parentNode.removeChild(el))")
   print("After remove ads")
+  print("First click to fire ads")
+  tc = driver.find_element(By.CSS_SELECTOR,'#header > div > div > div > div.col-sm-4 > div > a > img')
+  tc.click()
