92b2e7c92c211172f79f27412c1033469a323ec7
Lysech <arturuszynski13@gmail.com>
2023-03-05 14:00:50 +0100

    feat: add open site process to utils

--- utils.py
+++ utils.py
@@ -7,3 +7,19 @@ def hide_ads(driver):
   print("First click to fire ads!")
   tc = driver.find_element(By.CSS_SELECTOR,'#header > div > div > div > div.col-sm-4 > div > a > img')
   tc.click()
+
+
+def openSiteProcess():
+  print('Testing process start')
+  driver = webdriver.Chrome()
+  WebDriverWait(driver,10)
+  time.sleep(1)
+
+  driver.get('http://automationexercise.com')
+  WebDriverWait(driver,1)
+  time.sleep(1) 
+
+  opensite = driver.find_element(By.ID,'header')
+  time.sleep(1) 
+  if opensite.is_displayed():
+    print("The home page is visible successfully!") 
